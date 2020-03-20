9ce529b9d3f9b461e1494fc7597b5f9062951bbd
Václav Kubernát <sir.venceslas@gmail.com>
2020-03-20 20:17:09 +0100

    Frontend skeleton

--- src/prsi-middleware.ts
+++ src/prsi-middleware.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import path from "path";
+import ws from "express-ws";
+import Prsi from "./prsi-backend";
 
-const prsi = async (prefix = "", logger = (msg: string, _req?: express.Request) => console.log(msg)) => {
+const createPrsi = (prefix = "", logger = (msg: string, _req?: express.Request) => console.log(msg)) => {
     const router = express.Router();
+
+    router.use(prefix, express.static(path.join(__dirname + "/../dist"), {
+        dotfiles: "ignore",
+    }));
+
+    const prsi = new Prsi();
 
     return router;
 }
 
-export default prsi;
+export default createPrsi;

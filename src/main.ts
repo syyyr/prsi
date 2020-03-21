a07eb71f3282e372d7fb1ec7730f37aa38ad1db9
Václav Kubernát <sir.venceslas@gmail.com>
2020-03-21 12:06:18 +0100

    Assign ids to websockets and improve logging

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import express from "express";
 const wsApp = ws(express(), undefined, {
     leaveRouterUntouched: true
 });
-console.log("Initializing prsi...");
 const router = express.Router();
 wsApp.applyTo(router);
 wsApp.app.use(prsi(router));
-console.log("Prsi started.");
 wsApp.app.listen(3000);
 
 
--- src/prsi-middleware.ts
+++ src/prsi-middleware.ts
@@ -4,7 +4,7 @@ import path from "path";
 import ws from "express-ws";
 import {isPlayerRegistration, isPlayerInput, Response, ErrorResponse, FrontendState} from "./prsi-communication";
 
-let prsiLogger: (msg: string, req?: express.Request) => void;
+let prsiLogger: (msg: string, ws?: any) => void;
 const prsi = new Prsi();
 
 type AugmentedSocket = (WebSocket & {
@@ -12,6 +12,13 @@ type AugmentedSocket = (WebSocket & {
 });
 
 const openSockets: AugmentedSocket[] = [];
+
+const idGen = (function*(): Generator {
+    let id = 1;
+    while (true) {
+        yield id++;
+    }
+}());
 
 const buildFrontendStateFor = (player: string): FrontendState => {
     const state = prsi.state();
@@ -33,37 +40,53 @@ const processMessage = (ws: any, message: string): Response => {
     if (isPlayerRegistration(parsed)) {
         openSockets.push(ws);
         if (openSockets.some((socket) => socket.__private_name === parsed.registerPlayer)) {
-            prsiLogger(`"${parsed.registerPlayer}" already belongs to someone else.`);
+            prsiLogger(`"${parsed.registerPlayer}" already belongs to someone else.`, ws);
             return new ErrorResponse("Someone else owns this username.");
         }
 
         ws.__private_name = parsed.registerPlayer;
         prsi.registerPlayer(parsed.registerPlayer);
-        prsiLogger(`Registered "${parsed.registerPlayer}".`);
+        prsiLogger(`Registered "${parsed.registerPlayer}".`, ws);
         return buildFrontendStateFor(parsed.registerPlayer);
     }
 
     if (isPlayerInput(parsed)) {
         if (parsed.name !== ws.__private_name) {
-            prsiLogger(`${ws.__private_name}: tried to act as ${parsed.name}.`);
+            prsiLogger(`${ws.__private_name}: tried to act as ${parsed.name}.`, ws);
             return new ErrorResponse("Someone else owns this username.");
         }
 
         return buildFrontendStateFor(parsed.name);
     }
 
-    prsiLogger(`${ws.__private_name} tried to act as ${parsed.name}.`);
+    prsiLogger(`${ws.__private_name} tried to act as ${parsed.name}.`, ws);
     return new ErrorResponse("Invalid request.");
 };
 
 const createPrsi = (wsEnabledRouter: ws.Router, prefix = "", logger = (msg: string, _req?: express.Request) => console.log(msg)) => {
-    prsiLogger = logger;
+    prsiLogger = (msg: string, ws?: any) => {
+        // Manually inject the ws id. I know using `any` isn't the cleanest
+        // solution, but then again, the whole express-ws thing isn't very
+        // clean.
+        if (typeof ws !== "undefined") {
+            logger(msg, {
+                session: {
+                    id: `ws/${ws.__private_id}`
+                }
+            } as any);
+        } else {
+            logger(msg);
+        }
+    };
     wsEnabledRouter.use(prefix, express.static(path.join(__dirname + "/../dist"), {
         dotfiles: "ignore",
     }));
 
+    prsiLogger("Initializing prsi...");
+
     wsEnabledRouter.ws(prefix, (ws) => {
-        logger("New websocket.");
+        (ws as any).__private_id = idGen.next().value;
+        prsiLogger("New websocket.", ws);
         ws.on("message", (message) => {
             ws.send(JSON.stringify(processMessage(ws, message.toString())));
         });
@@ -72,16 +95,21 @@ const createPrsi = (wsEnabledRouter: ws.Router, prefix = "", logger = (msg: stri
             // Using Object here, because I want to compare references
             const closed = openSockets.findIndex((socket) => socket === ws as Object);
 
+            let name = "<unknown>";
             // Have to check whether the closing socket was already registered
             if (typeof openSockets[closed].__private_name !== "undefined") {
+                name = openSockets[closed].__private_name;
                 prsi.unregisterPlayer(openSockets[closed].__private_name);
             }
+
+            prsiLogger(`${name} disconnected.`, ws);
+
             openSockets.splice(closed, 1);
-            logger("Websocket closed.");
         });
     });
 
 
+    prsiLogger("Prsi initialized.");
     return wsEnabledRouter;
 }
 

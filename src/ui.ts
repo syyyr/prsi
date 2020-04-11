46be619fa6e243998c8baefad8238ed33a0a67df
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-11 21:25:31 +0200

    ui: Refactor PlayerBox into a component

--- src/ui.ts
+++ src/ui.ts
@@ -53,6 +53,67 @@ class DrawButton extends React.Component<Renderer> {
     }
 }
 
+const renderPlace = (place: Place): React.ReactNode => {
+    return React.createElement("div", null, (() => {
+        switch (place) {
+            case Place.First:
+                return "První";
+            case Place.Second:
+                return "Druhej";
+            case Place.Third:
+                return "Třetí";
+            case Place.Fourth:
+                return "Čtvrtej";
+            case Place.Fifth:
+                return "Pátej";
+            case Place.Sixth:
+                return "Šeštej";
+        }
+    })());
+}
+
+interface PlayerBoxProps {
+    thisName: string;
+    players: string[];
+    whoseTurn?: string;
+    playerInfo?: {[key in string]: {cards?: number, place?: Place}};
+}
+
+class PlayerBox extends React.Component<PlayerBoxProps> {
+    render(): React.ReactNode {
+        const players = this.props.players;
+        const playerInfo = this.props.playerInfo;
+        const whoseTurn = this.props.whoseTurn;
+        return React.createElement("div", {key: "players", className: "flex-row"},
+            [
+                ...players.map((player) => {
+                    let playerInfoRender: undefined | React.ReactNode = undefined;
+                    if (typeof playerInfo !== "undefined" && typeof playerInfo[player] !== "undefined") {
+                        if (typeof playerInfo[player]!.cards === "number") {
+                            playerInfoRender = React.createElement("div",
+                                {className: "flex-row cardBacks-container"},
+                                // I have no idea why Typescript complains without an `!`
+                                Array.from({length: playerInfo[player].cards!}).map((_value, index) => renderCardBack(`card:${player}${index}`)))
+                        } else {
+                            playerInfoRender = renderPlace(playerInfo[player].place!);
+                        }
+                    }
+                    return [
+                        React.createElement("div", {className: "flex-column player-container"}, [
+                            React.createElement(Player, {
+                                name: this.props.thisName === player ? `${player} (ty)` : player,
+                                key: player,
+                                shouldEmphasize: typeof whoseTurn !== "undefined" && whoseTurn === player}, null
+                            ),
+                            playerInfoRender
+                        ])
+                    ];
+                }),
+            ]
+        )
+    }
+}
+
 interface YouOther {
     you: string;
     other: string;
@@ -144,6 +205,10 @@ class CardBack extends React.Component {
     }
 }
 
+const renderCardBack = (key: string): React.ReactNode => {
+    return React.createElement(CardBack, {key}, null);
+}
+
 class CardComponent extends React.Component<{card: Card, options?: CardOptions}> {
     render(): React.ReactNode {
         const options = this.props.options;
@@ -199,59 +264,6 @@ export class UI extends React.Component<{ws: any, thisName: string}, {gameState?
                 this.setState({picker: null});
             }
         }, dialogContent);
-    }
-
-    private renderCardBack(key: string): React.ReactNode {
-        return React.createElement(CardBack, {key}, null);
-    }
-
-    private renderPlace(place: Place): React.ReactNode {
-        return React.createElement("div", null, (() => {
-            switch (place) {
-                case Place.First:
-                    return "První";
-                case Place.Second:
-                    return "Druhej";
-                case Place.Third:
-                    return "Třetí";
-                case Place.Fourth:
-                    return "Čtvrtej";
-                case Place.Fifth:
-                    return "Pátej";
-                case Place.Sixth:
-                    return "Šeštej";
-            }
-        })());
-    }
-
-    renderPlayers(players: string[], whoseTurn?: string, playerInfo?: {[key in string]: {cards?: number, place?: Place}}): React.ReactNode {
-        return React.createElement("div", {key: "players", className: "flex-row"},
-            [
-                ...players.map((player) => {
-                    let playerInfoRender: undefined | React.ReactNode = undefined;
-                    if (typeof playerInfo !== "undefined" && typeof playerInfo[player] !== "undefined") {
-                        if (typeof playerInfo[player]!.cards === "number") {
-                            playerInfoRender = React.createElement("div",
-                                {className: "flex-row cardBacks-container"},
-                                // I have no idea why Typescript complains without an `!`
-                                Array.from({length: playerInfo[player].cards!}).map((_value, index) => this.renderCardBack(`card:${player}${index}`)))
-                        } else {
-                            playerInfoRender = this.renderPlace(playerInfo[player].place!);
-                        }
-                    }
-                    return [
-                        React.createElement("div", {className: "flex-column player-container"}, [
-                            React.createElement(Player, {
-                                name: this.props.thisName === player ? `${player} (ty)` : player,
-                                key: player,
-                                shouldEmphasize: typeof whoseTurn !== "undefined" && whoseTurn === player}, null
-                            ),
-                            playerInfoRender
-                        ])
-                    ];
-                }),
-            ]
-        )
     }
 
     readonly drawButtonString = {
@@ -476,7 +488,11 @@ export class UI extends React.Component<{ws: any, thisName: string}, {gameState?
         if (this.state.gameState.gameStarted === "no" && this.state.gameState.players.length >= 2) {
             elems.push(this.renderStartButton());
         }
-        elems.push(this.renderPlayers(this.state.gameState.players, this.state.gameState.gameInfo?.who, this.state.gameState.gameInfo?.playerInfo));
+        elems.push(React.createElement(PlayerBox, {
+            thisName: this.props.thisName,
+            players: this.state.gameState.players,
+            playerInfo: this.state.gameState.gameInfo?.playerInfo
+        }));
 
         if (typeof this.state.gameState.gameInfo === "undefined") {
             elems.push(this.renderPrompt("Hra nezačala."));

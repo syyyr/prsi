e7465535694e31f42e7104ed8b2853ceaf7b8758
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-12 23:29:47 +0200

    frontend: Refactor strings to another file

--- src/client/strings.ts
+++ src/client/strings.ts
@@ -1,6 +1,116 @@
-import {Color, Value} from "../common/types"
+import {ActionType, Color, Value, LastAction, Status} from "../common/types"
 
-export const CARDS_GENITIVE: {[key in keyof typeof Color]: {[key2 in keyof typeof Value]: string}} = {
+interface YouOther {
+    you: string;
+    other: string;
+}
+
+class InstructionStrings {
+    [Status.Ok]: YouOther = {you: "ERROR: Ok/you", other: "ERROR: Ok/other"};
+    [Status.PlayerMismatch]: YouOther = {you: "ERROR: PlayerMismatch/you", other: "ERROR: PlayerMismatch/other"};
+    [Status.CardMismatch]: YouOther = {you: "ERROR: CardMismatch/you", other: "ERROR: CardMismatch/other"};
+    [Status.ActionMismatch]: YouOther = {you: "ERROR: ActionMismatch/you", other: "ERROR: ActionMismatch/other"};
+    [Status.DontHaveCard]: YouOther = {you: "ERROR: DontHaveCard/you", other: "ERROR: DontHaveCard/other"};
+    [Status.NotAnAce]: YouOther = {you: "ERROR: NotAnAce/you", other: "ERROR: NotAnAce/other"};
+    [Status.NotASeven]: YouOther = {you: "ERROR: NotASeven/you", other: "ERROR: NotASeven/other"};
+    [Status.MustShuffle]: YouOther = {you: "ERROR: MustShuffle/you", other: "ERROR: MustShuffle/other"};
+    constructor(overrides?: InstructionOverride) {
+        if (typeof overrides === "undefined") {
+            return;
+        }
+
+        Object.keys(overrides).forEach((key: string) => {
+            const actualKey = key as Status; // TODO: check if Object.keys can return the enum
+            const override = overrides[actualKey]!;
+            if (typeof override.you !== "undefined") {
+                this[actualKey].you = override.you;
+            }
+            if (typeof override.other !== "undefined") {
+                this[actualKey].other = override.other;
+            }
+        });
+    }
+}
+
+export const colorStrings = {
+    [Color.Kule]: "kule",
+    [Color.Listy]: "listy",
+    [Color.Srdce]: "srdce",
+    [Color.Zaludy]: "žaludy",
+}
+
+export const values = {
+    [Value.Sedmicka]: "sedmu",
+    [Value.Osmicka]: "osmičku",
+    [Value.Devitka]: "devítku",
+    [Value.Desitka]: "desítku",
+    [Value.Spodek]: "spodka",
+    [Value.Svrsek]: "svrška",
+    [Value.Kral]: "krále",
+    [Value.Eso]: "eso",
+}
+
+const genPlayColor = (color: Color): InstructionStrings => {
+    return new InstructionStrings({
+        [Status.Ok]: {you: `Hraješ. (${colorStrings[color]})`, other: "@PLAYERNAME@ hraje."},
+        [Status.CardMismatch]: {you: `Tohle tam nemůžeš dát. Musíš zahrát ${colorStrings[color]}.`},
+        [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
+    });
+}
+
+const genPlaySeven = (drawCount: string): InstructionStrings => {
+    return new InstructionStrings({
+        [Status.Ok]: {you: `Lížeš ${drawCount}${drawCount !== "osm" ? ", nebo zahraj sedmu" : ""}.`, other: "@PLAYERNAME@ hraje."},
+        [Status.NotASeven]: {you: `Tohle tam nemůžeš dát. Lízej ${drawCount}${drawCount !== "osm" ? ", nebo zahraj sedmu" : ""}.`},
+        [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
+    });
+}
+
+export const instructionStrings: {[key in keyof typeof ActionType]: InstructionStrings} = {
+    [ActionType.Play]: new InstructionStrings({
+        [Status.Ok]: {you: "Hraješ.", other: "@PLAYERNAME@ hraje."},
+        [Status.CardMismatch]: {you: "Tohle tam nemůžeš dát. Musíš zahrát @TOPVALUE@ nebo @TOPCOLOR@ (nebo si lízni)."},
+        [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."},
+        [Status.MustShuffle]: {you: "Hra skončila, musíš zamíchat.", other: "Hra skončila, @PLAYERNAME@ musí zamíchat."}
+    }),
+    [ActionType.PlayKule]: genPlayColor(Color.Kule),
+    [ActionType.PlayListy]: genPlayColor(Color.Listy),
+    [ActionType.PlayZaludy]: genPlayColor(Color.Zaludy),
+    [ActionType.PlaySrdce]: genPlayColor(Color.Srdce),
+    [ActionType.Shuffle]: new InstructionStrings({
+        [Status.Ok]: {you: "Mícháš.", other: "Míchá @PLAYERNAME@."},
+        [Status.PlayerMismatch]: {other: "Ty nemícháš, míchá @PLAYERNAME@."},
+        [Status.MustShuffle]: {you: "Hra skončila, musíš zamíchat.", other: "Hra skončila, @PLAYERNAME@ musí zamíchat."}
+    }),
+    [ActionType.DrawTwo]: genPlaySeven("dvě"),
+    [ActionType.DrawFour]: genPlaySeven("čtyři"),
+    [ActionType.DrawSix]: genPlaySeven("šest"),
+    [ActionType.DrawEight]: genPlaySeven("osm"),
+    [ActionType.SkipTurn]: new InstructionStrings({
+        [Status.Ok]: {you: "Stojíš nebo zahraj eso.", other: "@PLAYERNAME@ hraje."},
+        [Status.NotAnAce]: {you: "Tohle tam nemůžeš dát. Buď stojíš, nebo zahraj eso."},
+        [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
+    }),
+};
+
+export const lastPlayStrings: {[key in keyof typeof LastAction]: YouOther} = {
+    [LastAction.Play]: {you: "Zahráls @CARDS_GENITIVE@.", other: "@PLAYERNAME@ zahrál @CARDS_GENITIVE@."},
+    [LastAction.SkipTurn]: {you: "Stojíš.", other: "@PLAYERNAME@ stojí."},
+    [LastAction.Draw]: {you: "Líznuls.", other: "@PLAYERNAME@ líznul."},
+    [LastAction.DrawTwo]: {you: "Líznuls dvě.", other: "@PLAYERNAME@ líznul dvě."},
+    [LastAction.DrawFour]: {you: "Líznuls čtyři.", other: "@PLAYERNAME@ líznul čtyři."},
+    [LastAction.DrawSix]: {you: "Líznuls šest.", other: "@PLAYERNAME@ líznul šest."},
+    [LastAction.DrawEight]: {you: "Líznuls osm.", other: "@PLAYERNAME@ líznul osm."},
+    [LastAction.Change]: {you: "Změnils na @COLORCHANGE@.", other: "@PLAYERNAME@ změnil na @COLORCHANGE@."},
+    [LastAction.Disconnect]: {you: "Odpojil ses? Tohle bys neměl vidět.", other: "@PLAYERNAME@ se odpojil."},
+    [LastAction.Return]: {you: "Zahráls červenou sedmičku a vrátils @RETURN@ do hry!", other: "@PLAYERNAME@ zahrál červenou sedmičku a vrátil @RETURN@ do hry!"},
+};
+
+type InstructionOverride = {
+    [key in keyof typeof Status]?: {you?: string, other?: string};
+}
+
+export const cardsGenitive: {[key in keyof typeof Color]: {[key2 in keyof typeof Value]: string}} = {
     [Color.Kule]: {
         [Value.Sedmicka]: "kulou sedmu",
         [Value.Osmicka]: "kulovou osmičku",
--- src/client/ui.ts
+++ src/client/ui.ts
@@ -8,44 +8,8 @@ import Prompt from "./components/prompt";
 import StartButton from "./components/startbutton";
 import Stats from "./components/stats";
 import Title from "./components/title";
-import {CARDS_GENITIVE} from "./strings";
+import {cardsGenitive, instructionStrings, lastPlayStrings, colorStrings, values} from "./strings";
 import {audio} from "./sounds";
-
-interface YouOther {
-    you: string;
-    other: string;
-}
-
-type InstructionOverride = {
-    [key in keyof typeof Status]?: {you?: string, other?: string};
-}
-
-class InstructionStrings {
-    [Status.Ok]: YouOther = {you: "ERROR: Ok/you", other: "ERROR: Ok/other"};
-    [Status.PlayerMismatch]: YouOther = {you: "ERROR: PlayerMismatch/you", other: "ERROR: PlayerMismatch/other"};
-    [Status.CardMismatch]: YouOther = {you: "ERROR: CardMismatch/you", other: "ERROR: CardMismatch/other"};
-    [Status.ActionMismatch]: YouOther = {you: "ERROR: ActionMismatch/you", other: "ERROR: ActionMismatch/other"};
-    [Status.DontHaveCard]: YouOther = {you: "ERROR: DontHaveCard/you", other: "ERROR: DontHaveCard/other"};
-    [Status.NotAnAce]: YouOther = {you: "ERROR: NotAnAce/you", other: "ERROR: NotAnAce/other"};
-    [Status.NotASeven]: YouOther = {you: "ERROR: NotASeven/you", other: "ERROR: NotASeven/other"};
-    [Status.MustShuffle]: YouOther = {you: "ERROR: MustShuffle/you", other: "ERROR: MustShuffle/other"};
-    constructor(overrides?: InstructionOverride) {
-        if (typeof overrides === "undefined") {
-            return;
-        }
-
-        Object.keys(overrides).forEach((key: string) => {
-            const actualKey = key as Status; // TODO: check if Object.keys can return the enum
-            const override = overrides[actualKey]!;
-            if (typeof override.you !== "undefined") {
-                this[actualKey].you = override.you;
-            }
-            if (typeof override.other !== "undefined") {
-                this[actualKey].other = override.other;
-            }
-        });
-    }
-}
 
 const startGame = (ws: any) => ws.send(JSON.stringify(new StartGame()));
 
@@ -69,97 +33,23 @@ export class UI extends React.Component<{ws: any, thisName: string}, {gameState?
         };
     }
 
-    readonly colorStrings = {
-        [Color.Kule]: "kule",
-        [Color.Listy]: "listy",
-        [Color.Srdce]: "srdce",
-        [Color.Zaludy]: "žaludy",
-    }
-
-    readonly values = {
-        [Value.Sedmicka]: "sedmu",
-        [Value.Osmicka]: "osmičku",
-        [Value.Devitka]: "devítku",
-        [Value.Desitka]: "desítku",
-        [Value.Spodek]: "spodka",
-        [Value.Svrsek]: "svrška",
-        [Value.Kral]: "krále",
-        [Value.Eso]: "eso",
-    }
-
-    genPlayColor(color: Color): InstructionStrings {
-        return new InstructionStrings({
-            [Status.Ok]: {you: `Hraješ. (${this.colorStrings[color]})`, other: "@PLAYERNAME@ hraje."},
-            [Status.CardMismatch]: {you: `Tohle tam nemůžeš dát. Musíš zahrát ${this.colorStrings[color]}.`},
-            [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
-        });
-    }
-
-    genPlaySeven(drawCount: string): InstructionStrings {
-        return new InstructionStrings({
-            [Status.Ok]: {you: `Lížeš ${drawCount}${drawCount !== "osm" ? ", nebo zahraj sedmu" : ""}.`, other: "@PLAYERNAME@ hraje."},
-            [Status.NotASeven]: {you: `Tohle tam nemůžeš dát. Lízej ${drawCount}${drawCount !== "osm" ? ", nebo zahraj sedmu" : ""}.`},
-            [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
-        });
-    }
-
-    readonly instructionStrings: {[key in keyof typeof ActionType]: InstructionStrings} = {
-        [ActionType.Play]: new InstructionStrings({
-            [Status.Ok]: {you: "Hraješ.", other: "@PLAYERNAME@ hraje."},
-            [Status.CardMismatch]: {you: "Tohle tam nemůžeš dát. Musíš zahrát @TOPVALUE@ nebo @TOPCOLOR@ (nebo si lízni)."},
-            [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."},
-            [Status.MustShuffle]: {you: "Hra skončila, musíš zamíchat.", other: "Hra skončila, @PLAYERNAME@ musí zamíchat."}
-        }),
-        [ActionType.PlayKule]: this.genPlayColor(Color.Kule),
-        [ActionType.PlayListy]: this.genPlayColor(Color.Listy),
-        [ActionType.PlayZaludy]: this.genPlayColor(Color.Zaludy),
-        [ActionType.PlaySrdce]: this.genPlayColor(Color.Srdce),
-        [ActionType.Shuffle]: new InstructionStrings({
-            [Status.Ok]: {you: "Mícháš.", other: "Míchá @PLAYERNAME@."},
-            [Status.PlayerMismatch]: {other: "Ty nemícháš, míchá @PLAYERNAME@."},
-            [Status.MustShuffle]: {you: "Hra skončila, musíš zamíchat.", other: "Hra skončila, @PLAYERNAME@ musí zamíchat."}
-        }),
-        [ActionType.DrawTwo]: this.genPlaySeven("dvě"),
-        [ActionType.DrawFour]: this.genPlaySeven("čtyři"),
-        [ActionType.DrawSix]: this.genPlaySeven("šest"),
-        [ActionType.DrawEight]: this.genPlaySeven("osm"),
-        [ActionType.SkipTurn]: new InstructionStrings({
-            [Status.Ok]: {you: "Stojíš nebo zahraj eso.", other: "@PLAYERNAME@ hraje."},
-            [Status.NotAnAce]: {you: "Tohle tam nemůžeš dát. Buď stojíš, nebo zahraj eso."},
-            [Status.PlayerMismatch]: {other: "Teď nehraješ, hraje @PLAYERNAME@."}
-        }),
-    };
-
-    readonly lastPlayStrings: {[key in keyof typeof LastAction]: YouOther} = {
-        [LastAction.Play]: {you: "Zahráls @CARDS_GENITIVE@.", other: "@PLAYERNAME@ zahrál @CARDS_GENITIVE@."},
-        [LastAction.SkipTurn]: {you: "Stojíš.", other: "@PLAYERNAME@ stojí."},
-        [LastAction.Draw]: {you: "Líznuls.", other: "@PLAYERNAME@ líznul."},
-        [LastAction.DrawTwo]: {you: "Líznuls dvě.", other: "@PLAYERNAME@ líznul dvě."},
-        [LastAction.DrawFour]: {you: "Líznuls čtyři.", other: "@PLAYERNAME@ líznul čtyři."},
-        [LastAction.DrawSix]: {you: "Líznuls šest.", other: "@PLAYERNAME@ líznul šest."},
-        [LastAction.DrawEight]: {you: "Líznuls osm.", other: "@PLAYERNAME@ líznul osm."},
-        [LastAction.Change]: {you: "Změnils na @COLORCHANGE@.", other: "@PLAYERNAME@ změnil na @COLORCHANGE@."},
-        [LastAction.Disconnect]: {you: "Odpojil ses? Tohle bys neměl vidět.", other: "@PLAYERNAME@ se odpojil."},
-        [LastAction.Return]: {you: "Zahráls červenou sedmičku a vrátils @RETURN@ do hry!", other: "@PLAYERNAME@ zahrál červenou sedmičku a vrátil @RETURN@ do hry!"},
-    };
-
     // FIXME: Check if this can be refactored inside the prompt
     buildInstructions(wantedAction: ActionType, status: Status, you: string, turn: string, topCard: Card, lastPlay?: LastPlay): string {
         const lastPlayStr = status !== Status.Ok || typeof lastPlay === "undefined" ? undefined :
-            this.lastPlayStrings[lastPlay.playerAction][you === lastPlay.who ? "you" : "other"]
+            lastPlayStrings[lastPlay.playerAction][you === lastPlay.who ? "you" : "other"]
             .replace("@PLAYERNAME@", lastPlay.who)
             .replace("@COLORCHANGE@", typeof lastPlay.playDetails === "undefined" ? "PLAYDETAILS unavailable" :
                 typeof lastPlay.playDetails.colorChange === "undefined" ? "COLORCHANGE unavailable" :
-                this.colorStrings[lastPlay.playDetails.colorChange])
+                colorStrings[lastPlay.playDetails.colorChange])
             .replace("@CARDS_GENITIVE@", typeof lastPlay.playDetails === "undefined"? "CARD unavailable" :
-                CARDS_GENITIVE[lastPlay.playDetails.card.color][lastPlay.playDetails.card.value])
+                cardsGenitive[lastPlay.playDetails.card.color][lastPlay.playDetails.card.value])
             .replace("@RETURN@", typeof lastPlay.playDetails === "undefined" || typeof lastPlay.playDetails.returned === "undefined" ? "RETURN unavailable" :
                 lastPlay.playDetails.returned)
             .replace(/\.$/, !lastPlay.didWin ? "." : lastPlay.who === you ? " a vyhráls." : " a vyhrál.");
-        const instructions = this.instructionStrings[wantedAction][status][you === turn ? "you" : "other"]
+        const instructions = instructionStrings[wantedAction][status][you === turn ? "you" : "other"]
             .replace("@PLAYERNAME@", turn)
-            .replace("@TOPCOLOR@", this.colorStrings[topCard.color])
-            .replace("@TOPVALUE@", this.values[topCard.value]);
+            .replace("@TOPCOLOR@", colorStrings[topCard.color])
+            .replace("@TOPVALUE@", values[topCard.value]);
         return `${typeof lastPlayStr !== "undefined" ? `${lastPlayStr} ` : ""}${instructions}`;
     }
 

b4fefcaca6936fa0e912e047ad8b6c9945ba286e
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-13 00:21:54 +0200

    frontend: Rework playerbox

--- src/client/components/playerbox.ts
+++ src/client/components/playerbox.ts
@@ -1,6 +1,4 @@
 import * as React from "react";
-import CardBack from "./cardback";
-import PlaceComponent from "./place";
 import Player from "./player";
 import {Place} from "../../common/types";
 
@@ -13,33 +11,16 @@ interface PlayerBoxProps {
 
 export default class extends React.Component<PlayerBoxProps> {
     render(): React.ReactNode {
-        const players = this.props.players;
         const playerInfo = this.props.playerInfo;
-        const whoseTurn = this.props.whoseTurn;
         return React.createElement("div", {key: "players", className: "flex-row"},
             [
-                ...players.map((player) => {
-                    let playerInfoRender: undefined | React.ReactNode = undefined;
-                    if (typeof playerInfo !== "undefined" && typeof playerInfo[player] !== "undefined") {
-                        if (typeof playerInfo[player]!.cards === "number") {
-                            playerInfoRender = React.createElement("div",
-                                {className: "flex-row cardBacks-container"},
-                                // I have no idea why Typescript complains without an `!`
-                                Array.from({length: playerInfo[player].cards!}).map((_value, index) => React.createElement(CardBack, {key: `card:${player}${index}`})));
-                        } else {
-                            playerInfoRender = React.createElement(PlaceComponent, {place: playerInfo[player].place!});
-                        }
-                    }
-                    return [
-                        React.createElement("div", {className: "flex-column player-container"}, [
-                            React.createElement(Player, {
-                                name: this.props.thisName === player ? `${player} (ty)` : player,
-                                key: player,
-                                shouldEmphasize: typeof whoseTurn !== "undefined" && whoseTurn === player}, null
-                            ),
-                            playerInfoRender
-                        ])
-                    ];
+                ...this.props.players.map((player) => {
+                    return React.createElement(Player, ({
+                        name: player,
+                        onTurn: this.props.whoseTurn === name,
+                        cards: playerInfo?.[player].cards,
+                        place: playerInfo?.[player].place
+                    }));
                 }),
             ]
         )

49dd4be591fe6d48b2914db83308b871e6fd6f71
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-13 01:42:43 +0200

    frontend: Refactor Hand to a file

---
+++ src/client/components/hand.ts
@@ -0,0 +1,23 @@
+import * as React from "react";
+import {Card, Color, Value} from "../../common/types";
+import CardComponent from "./card";
+
+export default class Hand extends React.Component<{hand: Card[], playCard: (card: Card) => void, openPicker: (svrsekColor: Color) => void}> {
+    render(): React.ReactNode {
+        return React.createElement("div", {className: "flex-row hand-container"}, this.props.hand.map((card, index) => React.createElement(CardComponent, {
+            key: `hand:${index}`,
+            card: card,
+            options: {
+                isBottomCard: "bottom",
+                halo: "halo",
+                onClick: () => {
+                    if (card.value === Value.Svrsek) {
+                        this.props.openPicker(card.color);
+                        return;
+                    }
+                    this.props.playCard(card);
+                }
+            }
+        })));
+    }
+}
--- src/client/components/playfield.ts
+++ src/client/components/playfield.ts
@@ -1,28 +1,9 @@
 import * as React from "react";
 import DrawButton from "./drawbutton";
 import {Card, Color, Value, ActionType, changeActionToColor} from "../../common/types";
-import {CardTooltip, drawButtonString} from "../strings";
+import {CardTooltip} from "../strings";
 import CardComponent from "./card";
-
-class Hand extends React.Component<{hand: Card[], playCard: (card: Card) => void, openPicker: (svrsekColor: Color) => void}> {
-    render(): React.ReactNode {
-        return React.createElement("div", {className: "flex-row hand-container"}, this.props.hand.map((card, index) => React.createElement(CardComponent, {
-            key: `hand:${index}`,
-            card: card,
-            options: {
-                isBottomCard: "bottom",
-                halo: "halo",
-                onClick: () => {
-                    if (card.value === Value.Svrsek) {
-                        this.props.openPicker(card.color);
-                        return;
-                    }
-                    this.props.playCard(card);
-                }
-            }
-        })));
-    }
-}
+import Hand from "./hand";
 
 const isColorChange = (action: ActionType) => {
     switch (action) {

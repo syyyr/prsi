958c07492dec7241f7b52c5fabfdbe57cc9f7140
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-13 00:34:28 +0200

    frontend: Fix table layout

--- src/client/components/stats.ts
+++ src/client/components/stats.ts
@@ -3,22 +3,25 @@ import {FrontendStats} from "../../common/communication";
 
 export default class extends React.Component<{stats: {[key in string]: FrontendStats}}> {
     render(): React.ReactNode {
-        // FIXME: fix table structure
         return React.createElement("table", {className: "statsTable"},
             [
-                React.createElement("thead", {className: "statsHeader"}, [
-                    React.createElement("th", {colSpan: "3"}, "Statistika"),
+                React.createElement("thead", null, [
+                    React.createElement("tr", null,
+                        React.createElement("th", {colSpan: "3"}, "Statistika")
+                    ),
                     React.createElement("tr", null, [
                         React.createElement("td", {className: "statsDesc"}, "Jméno"),
                         React.createElement("td", {className: "statsDesc"}, "Úspěšnost"),
                         React.createElement("td", {className: "statsDesc"}, "Odehráno")
-                    ]
-                )]),
-                ...Object.entries(this.props.stats).map(([player, stats]) => React.createElement("tr", null, [
-                    React.createElement("td", null, player),
-                    React.createElement("td", null, `${Math.round(stats.successRate * 100)} %`),
-                    React.createElement("td", null, stats.gamesPlayed),
-                ]))
+                    ])
+                ]),
+                React.createElement("tbody", {className: "statsHeader"}, [
+                    ...Object.entries(this.props.stats).map(([player, stats]) => React.createElement("tr", null, [
+                        React.createElement("td", null, player),
+                        React.createElement("td", null, `${Math.round(stats.successRate * 100)} %`),
+                        React.createElement("td", null, stats.gamesPlayed),
+                    ]))
+                ]),
             ]);
     }
 }

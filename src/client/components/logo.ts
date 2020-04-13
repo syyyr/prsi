8a8d21d6a28ed42bbc421a6e329934eb5865d021
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-13 19:27:20 +0200

    frontend: Rework logo image storage
    This is now in-line with new cardback image storage.

--- src/client/components/logo.ts
+++ src/client/components/logo.ts
@@ -1,7 +1,8 @@
 import * as React from "react";
+import image from "./logo-image";
 
 export default class Logo extends React.Component<{center: boolean}> {
     render(): React.ReactNode {
-        return React.createElement("img", {className: `playfield-logo${this.props.center ? " logo align-center" : ""}`});
+        return React.createElement("img", {src: image, className: `${this.props.center ? "logo align-center" : ""}`});
     }
 }

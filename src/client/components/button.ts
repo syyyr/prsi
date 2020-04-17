a1fe4205f598dd8de30c94a7d6faff71c56c9b3d
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-17 04:25:47 +0200

    frontend: Rename a class and fix key in Button

--- src/client/components/button.ts
+++ src/client/components/button.ts
@@ -3,9 +3,8 @@ import * as React from "react";
 export default class Button extends React.Component<{onClick: () => void, text: string}> {
     render(): React.ReactNode {
         return React.createElement("button", {
-            key: "nameInputButton",
             onClick: this.props.onClick,
-            className: "nameButton"
+            className: "fancyButton"
         }, this.props.text)
     }
 }

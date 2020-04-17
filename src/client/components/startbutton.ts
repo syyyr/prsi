5f8ed1124eb385d63da062322b2c99b25af6f372
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-17 04:32:14 +0200

    frontend: Use fancy start button

--- src/client/components/startbutton.ts
+++ src/client/components/startbutton.ts
@@ -1,12 +1,8 @@
 import * as React from "react";
+import Button from "./button";
 
 export default class StartButton extends React.Component<{startGame: () => void}> {
     render() {
-        return React.createElement(
-            "button",
-            {
-                onClick: this.props.startGame
-            },
-            "Start");
+        return React.createElement(Button, {onClick: this.props.startGame, text: "Start"});
     }
 }

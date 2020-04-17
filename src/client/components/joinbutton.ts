6b110c36d96d00e23cb0914572abbf3f108cce42
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-17 04:28:52 +0200

    frontend: Use fancy join button

--- src/client/components/joinbutton.ts
+++ src/client/components/joinbutton.ts
@@ -1,12 +1,8 @@
 import * as React from "react";
+import Button from "./button";
 
 export default class JoinButton extends React.Component<{openDialog: () => void}> {
     render() {
-        return React.createElement(
-            "button",
-            {
-                onClick: this.props.openDialog
-            },
-            "Přidat se");
+        return React.createElement(Button, {onClick: this.props.openDialog, text: "Přidat se"});
     }
 }

75c96eba94fb3eae2df6fe5d4625b45ef33d99e2
Václav Kubernát <sir.venceslas@gmail.com>
2020-04-18 14:44:52 +0200

    frontend: Change title and logo to PureComponents

--- src/client/components/logo.ts
+++ src/client/components/logo.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 import image from "../images/logo-image";
 
-export default class Logo extends React.Component {
+export default class Logo extends React.PureComponent {
     render(): React.ReactNode {
         return React.createElement("img", {src: image, className: "logo"});
     }
--- src/client/components/title.ts
+++ src/client/components/title.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Logo from "./logo";
 
-export default class Title extends React.Component {
+export default class Title extends React.PureComponent {
     render(): React.ReactNode {
         return React.createElement(Logo);
     }
